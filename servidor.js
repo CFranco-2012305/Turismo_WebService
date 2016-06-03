f5cab7c392c7b9e226f785b2034529eb5a903abc
cfranco-2012305 <cfranco-2012305@kinal.edu.gt>
2016-06-03 11:54:29 -0600

    index

--- servidor.js
+++ servidor.js
@@ -1,10 +1,18 @@
 var express=require('express');
-var bodyparser=require('body-parser');
+var bodyParser=require('body-parser');
 var morgan=require('morgan');
+var mysql=require('mysql');
 var puerto=3000;
 var app=express();
 
+app.use(bodyParser.urlencoded({
+	extended:false	
+}));
+
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use('/api/v1',require('./rutas'));
 
 app.listen(puerto,function(){
-	console.log("Servidor iniciado en el puerto numero"+puerto);
+	console.log("Servidor iniciado en el puerto numero "+puerto);
 });
