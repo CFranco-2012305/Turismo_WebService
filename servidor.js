19ccd7e8892cb4c33f536d0c1ba70e79fb9b50b7
cfranco-2012305 <cfranco-2012305@kinal.edu.gt>
2016-06-03 10:44:20 -0600

    12:12 servidor

---
+++ servidor.js
@@ -0,0 +1,10 @@
+var express=require('express');
+var bodyparser=require('body-parser');
+var morgan=require('morgan');
+var puerto=3000;
+var app=express();
+
+
+app.listen(puerto,function(){
+	console.log("Servidor iniciado en el puerto numero"+puerto);
+})
