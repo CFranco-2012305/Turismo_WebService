62006a9cf282a24a7cc20f04ca9b8c4672af9cc6
cfranco-2012305 <cfranco-2012305@kinal.edu.gt>
2016-06-06 08:15:15 -0600

    cotrolador

---
+++ contoller/ControlladorUsuario.js
@@ -0,0 +1,6 @@
+var usuario={
+    registro:function(peticion,Respuesta){
+        respuesta.send("Servicio Iniciado");
+    }
+};
+module.exports=usuario;
--- rutas/index.js
+++ rutas/index.js
@@ -1,6 +1,7 @@
 var ruta=require('express').Router();
+var usuario=require('../contoller/ControlladorUsuario.js')
 ruta.get('/',function(peticion,respuesta){
 	respuesta.send("Servidor iniciado");
 });
-
+ruta.get('/usuario/registro',usuario.registro);
 module.exports=ruta;
